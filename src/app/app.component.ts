27d2f6be6cb8c0ffa37036f8b3ff6e75347e45fc
Rahul kumar M S <rahulmanakyalath@gmail.com>
2022-01-11 07:28:59 +0530

    fully working ui v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor( private http:HttpClient){}
   title = 'RkMail';
+  onSubmit(data)
+  {
+    this.http.post('http://localhost:8089/spring-mail-app/api/v1/sendmail/',data)
+    .subscribe((result)=>{
+    console.warn("result",result)
+    })
+  }
 }
